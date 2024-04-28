1b387ad92772af87af9230e027327b512454dd9a
Bruno Viscardi <bruno.viscardi33@gmail.com>
2024-04-28 19:26:41 -0300

    feat: redicionamento do header

--- src/components/molecules/header/header.jsx
+++ src/components/molecules/header/header.jsx
@@ -2,6 +2,7 @@ import "./header.css"
 import { useState, useContext} from "react";
 import { Link, useParams } from 'react-router-dom';
 import { UsuarioContext } from "../../../context/UsuarioContext";
+import { useEffect } from "react";
 
 
 
@@ -9,18 +10,36 @@ function Header() {
     const {idUser} = useParams()
     const {usuarios}= useContext(UsuarioContext)
 
+
+    useEffect(() => {
+        const usuarioLogado = usuarios.find(pessoa=> pessoa.id == idUser)
+        if (usuarioLogado) {
+            setNome({ nome: usuarioLogado.nome})
+            console.log(usuarioLogado.nome)
+            console.log(idUser)
+        }        
+    }, [idUser, usuarios])
+
+    
+
+    const [nome, setNome] = useState({
+        nome: ""
+    })
+
+    
+
     
 
     return (
         <nav className="NavBar">
             <div>
-                <Link to="/:idUser">Nome do projeto</Link>
-                <Link to="/:idUser/listagem-exercicio">Cadastrar locais</Link>
-                <Link to="/:idUser/cadastro-exercicio">Editar locais</Link>
+                <Link to={`/${idUser}`}>Nome do projeto</Link>
+                <Link to={`/${idUser}/listagem-exercicio`}>Cadastrar locais</Link>
+                <Link to={`/${idUser}/cadastro-exercicio`}>Editar locais</Link>
             </div>
 
             <div>
-                <p>Olá, </p>
+                <p>Olá, {nome.nome}</p>
                 <Link to="/login">Sair</Link>
             </div>
         </nav>
