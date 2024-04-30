f7002133d3d90c15ec4572b756d36e7caad4d127
Bruno Viscardi <bruno.viscardi33@gmail.com>
2024-04-29 21:59:27 -0300

    feat: criação de função de deletar local

--- src/components/molecules/cardEditarLocais/cardEditarLocais.jsx
+++ src/components/molecules/cardEditarLocais/cardEditarLocais.jsx
@@ -1,17 +1,23 @@
 import "./cardEditarLocais.css"
+import { useContext } from "react";
+import {ExercicioContext} from "../../../context/ExercicioContext"
+
+
 
 function CardEditarLocais({ locais }) {
+
+    const {deletarExercicio}= useContext(ExercicioContext)
 
     return (
         <div className="card-Conteudo">
             <div className="CardArea">
                 <span className="titulo">{locais.nome} - {locais.cidade} / {locais.estado}</span>
                 <div className="divider"></div>
-                <span className="usuario">Por: {locais.tipo}</span>                
+                <span className="tipoAtividade">Tipo de atividade: {locais.tipo}</span>                
             </div>
             <div className="cardAcoesEditar">
                 <button> Editar </button>
-                <button> Apagar </button>
+                <button onClick={()=>deletarExercicio(locais.id)}> Apagar </button>
             </div>
         </div>
     )
